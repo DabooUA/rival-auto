23637f3be9a72d311c39ea47d80a93823536f907
DabooUA <sbardysh@tutanota.com>
2021-12-07 15:57:47 -0500

    working on index.html

--- front_end/index.js
+++ front_end/index.js
@@ -2,20 +2,22 @@ import "bootstrap"
 import "../stylesheets/application"
 
 const base_url = "http://127.0.0.1:3000"
-const vehicleService = new VehicleService(base_url)
-const categoryService = new CategoryService(base_url)
+const vehicleService = new vehicleService(base_url)
+const categoryService = new categoryService(base_url)
 
-// Vehicle.vehicleForm.addEventListener('submit', handleSubmit)
-// vehicleService.getVehicles()
+Vehicle.vehicleForm.addEventListener('submit', handleSubmit)
+vehicleService.getVehicles()
 Vehicle.renderForm()
 
 Category.categoryForm.addEventListener('submit', handleSubmit)
 categoryService.getCategories()
 Category.renderForm()
 
+
+
 function handleSubmit(){
   event.preventDefault()
-  vehicleService.createVehicle()
+  categoryService.createCategory()
   event.target.reset()
 }
 
--- front_end/src/comparison.js
+++ front_end/src/comparison.js
@@ -1,7 +1,39 @@
 class Comparison{
+  
+  static all = []
+  static comparisonContainer = document.getElementById("comperisons-contrainer")
+  
   constructor({id, category_id, vehicle_id}){
     this.id = id
     this.category_id = category_id
     this.vehicle_id = vehicle_id
+
+    this.element = document.createElement("ul")
+    this.element.dataset.id = this.id
+    this.element.id = `comparison-${this.id}`
+    this.element.addEventListener("click", this.handleClick)
+
+    Comparison.all.push(this)
   }
+
+  comperisonHTML(){
+    this.element.innerHTML +=`
+    <div>
+      <h2>${this.vehicle_id} - ${this.vehilce_id}
+      </div>
+    `
+    return this.element
+  }
+
+  addOnDom(){
+    Comparison.comparisonContainer.append(this.comperisonHTML())
+  }
+
+  handleClick = () =>{
+    if (event.target.innerHTML === "Delete"){
+      this.element.remove()
+      comperisonService.deleteComperison(this.id)
+    }
+  }
+  
 }
